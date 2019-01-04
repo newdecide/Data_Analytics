fdf5103f3c8fbfb0b2bd6e42ee55133921666659
Hwang Si Yeon <lowgiant@gmail.com>
2019-01-04 23:45:37 +0900

    c3-1

---
+++ Python/Basic/ch3/console.py
@@ -0,0 +1,15 @@
+
+#input 입력을 받아서 저장을 하는 개념
+print("Enter your name:")
+somebody = input()
+print("hi", somebody)
+
+#문자를 찍으면 한 칸 뛰어서 됨. 
+print("Hello World", "Hello Again")
+
+temperator = float(input("온도를 입력하세요 :"))
+print(temperator)
+print(type((temperator)))
+
+
+
