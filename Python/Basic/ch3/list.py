d55cb3c6719642aad354f99c106cf587c72e0e6a
Hwang Si Yeon <lowgiant@gmail.com>
2019-01-06 22:45:23 +0900

    ch3 end

--- Python/Basic/ch3/list.py
+++ Python/Basic/ch3/list.py
@@ -13,4 +13,35 @@ print(cities[:])
 print(cities[-50:50])
 print(cities[::2],"and", cities[::-1])
 
-#9min
+#길이 
+print(len(color))
+
+#리스트 값 변경
+colors[0] = "yellow"
+print(colors)
+
+#리스트 추가
+colors.append("red")
+print(colors)
+
+colros2 = ["orange"]
+
+#값 자체가 변홤 
+colors.extxend(colors2)
+
+#값 삭제 
+del colors[0]
+
+#변수의 타입은 실행 시점에 됨.
+
+#패킹   
+t = [1,2,3]
+
+#언패킹
+a, b, c = t
+
+print(a,b,c)
+
+
+
+
