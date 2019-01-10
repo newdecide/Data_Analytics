2d5f61c59d85872a1db861138a68405f198677aa
Hwang Si Yeon <lowgiant@gmail.com>
2019-01-10 23:36:10 +0900

    ch4-3

--- Python/Basic/ch4/loop.py
+++ Python/Basic/ch4/loop.py
@@ -4,3 +4,22 @@ for looper in [1,2,3,4,5]:
     print("hello")
 for looper in range(0,5):
     print("hello")
+for i in range(1, 10, 2):
+    print(i)
+
+for i in range(10,1,-1):
+    print(i)
+
+#while 문(~동안)
+i = 1
+while i < 10:
+    print(i)
+    i+1 =1
+
+for i in range(0,5):
+    print(i)
+
+i=0
+while i < 5:
+    print(i)
+    i = i + 1    
