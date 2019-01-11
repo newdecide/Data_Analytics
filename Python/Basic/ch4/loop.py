d3df3d0661c63875a2ff65fd7611c51e199efe75
Hwang Si Yeon <lowgiant@gmail.com>
2019-01-11 23:41:40 +0900

    ch4-4

--- Python/Basic/ch4/loop.py
+++ Python/Basic/ch4/loop.py
@@ -11,10 +11,10 @@ for i in range(10,1,-1):
     print(i)
 
 #while 문(~동안)
-i = 1
-while i < 10:
-    print(i)
-    i+1 =1
+# i = 1
+# while i < 10:
+#     print(i)
+#    # i+1
 
 for i in range(0,5):
     print(i)
@@ -23,3 +23,16 @@ i=0
 while i < 5:
     print(i)
     i = i + 1    
+
+#break 문
+for i in range(10):
+    if i == 5: break
+    print(i)
+print("EOP")
+
+#continue 특정 조전에서 남은 반복 명령  
+for i in range(10):
+    if i == 5: continue
+    print(i)
+print("EOP")
+
