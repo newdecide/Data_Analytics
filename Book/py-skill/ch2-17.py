fbd90425d1ca7174abcb79c83bb6e5199f3d3de4
lowgiant <lowgiant@gmail.com>
2019-03-18 22:35:54 +0900

    py-skill-11

--- Book/py-skill/ch2-17.py
+++ Book/py-skill/ch2-17.py
@@ -5,4 +5,5 @@ import matplotlib
 import pandas as pd
 import numpy 
 
-pd = 
+pd = 0
+print(pd)
--- Book/py-skill/tempCodeRunnerFile.py
+++ Book/py-skill/tempCodeRunnerFile.py
@@ -1,14 +1,38 @@
-#16. 리스트를 반환하는 대신 제너레이터를 고려하자
+#이터레이터를 병렬로 처리하면 zip을 사용하자
 
-def index_words(text):
-    result = []
-    if text:
-        result.append(0)
-    for index, letter in enumerate(text):
-        if letter == ' ':
-            result.append(index + 1)
-    return result
+#1) 리스트 컴프리헨션
+names = ['Cecilia', 'Lise', 'Marie']
+letters = [len(n) for n in names]
+print("c1:",letters)
 
-address = 'Four score and seven years ago...'
-result = index_words(address)
-print(result[:3])
+#2)if
+longest_name = None
+max_letters = 0
+for i in range(len(names)):
+    count = letters[i]
+    if count > max_letters:
+        longest_name = names[i]
+        max_letters = count
+
+print("c2:", longest_name)
+
+#3) enumerate
+for i , name in enumerate(names):
+    count = letters[i]
+    if count > max_letters:
+        longest_name = name
+        max_letters = count
+
+print("c3:" ,longest_name)
+
+#4) zip => 튜플로 저장 됨.
+#제너레이터가 아니기 때문에 반환하는 작업을 해야함
+for name, count in zip(names, letters):
+    if count > max_letters:
+        longest_name = name
+        max_letters = count
+
+#5) 추가 한 값이 아닌 리스트 컴프리헨션한 값만 들어온 것을 알 수 있음.
+names.append('Rosalind')
+for name, count in zip(names, letters):
+    print("c5:", name)
