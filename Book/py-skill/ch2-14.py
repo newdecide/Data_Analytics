062418ea957e799a773021f06777ee9e6f91a641
lowgiant <lowgiant@gmail.com>
2019-04-05 23:05:17 +0900

    py-skill-19

--- Book/py-skill/ch2-14.py
+++ Book/py-skill/ch2-14.py
@@ -1,16 +1,16 @@
 #None을 반환하기 보다는 예외를 일으키자
 # 21:55~ 22:12
 
-#1. 숫자를 나누는 헬버함수
-# def divide(a, b):
-#     try:
-#         return a /b
-#     except ZeroDivisionError:
-#         return None
+1. 숫자를 나누는 헬버함수
+def divide(a, b):
+    try:
+        return a /b
+    except ZeroDivisionError:
+        return None
 
-# result = divide(x, y)
-# if result is None:
-#     print('ch1 :','Invalid inputs')
+result = divide(x, y)
+if result is None:
+    print('ch1 :','Invalid inputs')
 
 2. 0이 되어버림
 
