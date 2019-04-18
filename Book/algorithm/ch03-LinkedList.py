079a2b23e95f8a4d97337158badd5a5d51858c2e
lowgiant <lowgiant@gmail.com>
2019-04-18 21:13:19 +0900

    algorithm-2

--- Book/algorithm/ch03-LinkedList.py
+++ Book/algorithm/ch03-LinkedList.py
@@ -1,1 +1,29 @@
+# Linked list 
+# 20190417 14:20 ~ 14:40
 
+class Node:
+    def __init__(self, data, next=None):
+        self.data=data
+        self.next=next
+
+def init_list():
+    global node_A
+    node_A = Node("A")
+    node_B = Node("B")
+    node_C = Node("C")
+    node_D = Node("D")
+    node_A.next = node_B
+    node_B.next = node_C
+    node_C.next = node_D
+
+def print_list():
+    global node_A
+    node = node_A
+    while node:
+        print(node.data)
+        node = node.next
+    print
+
+if __name__ == '__main__':
+    init_list()
+    print_list()
