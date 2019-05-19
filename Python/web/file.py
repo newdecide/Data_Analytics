8ed091d144844036fe509f5cc95fd1e8de3292b2
lowgiant <lowgiant@gmail.com>
2019-05-19 23:12:42 +0900

    py-web-01

--- Python/web/file.py
+++ Python/web/file.py
@@ -1,2 +1,32 @@
+#coding=utf-8
+
 print(1 + 3)
 print("Hello World")
+
+if 3 > 5:
+    print("3은 5보다 큽니다")
+
+print("여기는?")
+
+#1-06 문제 랜덤 숫자 맞추기
+
+# import random 
+
+# n = random.randint(1,100)
+# print(n)
+
+# s = int(input())
+
+# d = n - s 
+
+# if n == s:
+#     print("정답")
+# elif d <= 10:
+#     print("아깝네요")
+# elif d >= 10:
+#     print("틀렸습니다.")
+
+# #1-06(2) 
+
+for s in range(1,101):
+    print(s)
