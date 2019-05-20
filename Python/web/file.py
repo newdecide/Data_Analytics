c8144ed55083bc7d161e0006559e30b0e5f8eb01
lowgiant <lowgiant@gmail.com>
2019-05-20 20:10:51 +0900

    py-web-02

--- Python/web/file.py
+++ Python/web/file.py
@@ -10,23 +10,28 @@ print("여기는?")
 
 #1-06 문제 랜덤 숫자 맞추기
 
-# import random 
+import random 
 
-# n = random.randint(1,100)
-# print(n)
+n = random.randint(1,100)
+print(n)
 
-# s = int(input())
+s = int(input())
 
-# d = n - s 
+d = n - s 
 
-# if n == s:
-#     print("정답")
-# elif d <= 10:
-#     print("아깝네요")
-# elif d >= 10:
-#     print("틀렸습니다.")
+if n == s:
+    print("정답")
+elif d <= 10:
+    print("아깝네요")
+elif d >= 10:
+    print("틀렸습니다.")
 
 # #1-06(2) 
-
+#1 부터 100까지 출력하시오
 for s in range(1,101):
     print(s)
+
+
+
+while True:
+    print("실행")
